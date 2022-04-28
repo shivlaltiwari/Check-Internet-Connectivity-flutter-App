5ccd3bebbae3beeed4fe99f8ac7a37b00d2e2147
shiva tiwari <st172239@gmail.com>
2022-04-28 19:58:21 +0545

    on success toast msg used

--- lib/check_internet_controller.dart
+++ lib/check_internet_controller.dart
@@ -1,18 +1,18 @@
-
-
 import 'dart:io';
 
 import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
 
-class CheckInternet extends GetxController{
+class CheckInternet extends GetxController {
   Connectivity _connectivity = new Connectivity();
 
   bool? isOnline = false;
   bool get isMeOnline => isOnline!;
-  
-   @override
+
+  @override
   void onInit() {
     super.onInit();
     startMonitoring();
@@ -41,10 +41,18 @@ class CheckInternet extends GetxController{
 
       if (status == ConnectivityResult.none) {
         isOnline = false;
-         update();
+        update();
       } else {
         isOnline = true;
-         update();
+        Fluttertoast.showToast(
+            msg: "Wow!! Internet Connected",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            timeInSecForIosWeb: 1,
+            backgroundColor: Colors.green,
+            textColor: Colors.white,
+            fontSize: 16.0);
+        update();
       }
     } on PlatformException catch (e) {
       print("PlatformException: " + e.toString());
@@ -58,6 +66,14 @@ class CheckInternet extends GetxController{
           await InternetAddress.lookup('google.com');
       if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
         isConnected = true;
+        Fluttertoast.showToast(
+            msg: "Wow!! Internet Connected",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            timeInSecForIosWeb: 1,
+            backgroundColor: Colors.green,
+            textColor: Colors.white,
+            fontSize: 16.0);
       }
     } on SocketException catch (_) {
       isConnected = false;
